118b0c41d78b38757a569d18bc7eca53aa41188d
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2021-06-23 17:32:34 -0700

    wrote reducers for categories

--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,17 @@
 // this is like the contract agency
+
+import { createStore, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import categories from './categories.js';
+// import products from './products.js';
+
+const reducers = combineReducers({
+  categories,
+});
+
+const store = () => {
+  return createStore(reducers, composeWithDevTools());
+}
+
+export default store();
